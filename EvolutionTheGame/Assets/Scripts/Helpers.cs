765cad6d80dfd4ab44dd11b9c700018b52a45637
Adam <adamgryu@gmail.com>
2014-08-08 19:57:01 -0400

    Baby you spin me round right round

--- EvolutionTheGame/Assets/Scripts/Helpers.cs
+++ EvolutionTheGame/Assets/Scripts/Helpers.cs
@@ -2,7 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 public static class Helpers {
-	
+	public static Vector3 Flatten(this Vector3 vector) {
+		return new Vector3(vector.x, vector.y, 0);
+	}
+
+	public static Vector2 ToVector2(this Vector3 vector) {
+		return new Vector2(vector.x, vector.y);
+	}
+
+	public static Vector2 Rotate90DegreesCounterClockwise(this Vector2 vector) {
+		return new Vector2(-vector.y, vector.x);
+	}
 }
--- EvolutionTheGame/Assets/Scripts/Player.cs
+++ EvolutionTheGame/Assets/Scripts/Player.cs
@@ -16,9 +16,14 @@ public class Player : MonoBehaviour {
 			if (!this.prevMousePosition.HasValue) {
 				this.prevMousePosition = Input.mousePosition;
 			}
-			Vector3 worldMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			Vector3 worldMousePrev = Camera.main.ScreenToWorldPoint(this.prevMousePosition.Value);
-			this.transform.position += worldMouse - worldMousePrev;
+
+			Vector3 worldMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition).Flatten();
+			Vector3 worldMousePrev = Camera.main.ScreenToWorldPoint(this.prevMousePosition.Value).Flatten();
+			Vector3 prevAngleV = worldMouse - this.transform.position;
+			Vector3 angleV = worldMousePrev - this.transform.position;
+			float angle = Vector3.Angle(prevAngleV, angleV) * Mathf.Sign(Vector3.Dot(prevAngleV, angleV.ToVector2().Rotate90DegreesCounterClockwise()));
+			this.transform.Rotate(new Vector3(0, 0, 1), angle);
+
 			this.prevMousePosition = Input.mousePosition;
 		} else {
 			this.prevMousePosition = null;
