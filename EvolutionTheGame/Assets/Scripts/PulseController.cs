967775aa222ada456ad939dcca67b1125766a041
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-08-08 21:34:57 -0400

    fix bug

--- EvolutionTheGame/Assets/Scripts/PulseController.cs
+++ EvolutionTheGame/Assets/Scripts/PulseController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class PulseController : MonoBehaviour {
 
 	public AudioSource song;
+	public int bpm;
 
 	private Pulser[] pulsers;
 	private int samplesElapsed = 0;
