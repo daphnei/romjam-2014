5089c78e162acc5694fcfa472cb3cca34e851088
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-08-09 15:56:42 -0400

    Expose background cycle values.

--- Growth/Assets/Scripts/BackgroundCycler.cs
+++ Growth/Assets/Scripts/BackgroundCycler.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class BackgroundCycler : MonoBehaviour {
 
 	public float frequency = 0.1f;
+	public int redMax = 255;
+	public int greenMax = 255;
+	public int blueMax = 255;
 
 	// Use this for initialization
 	void Start () {
@@ -13,9 +16,9 @@ public class BackgroundCycler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Camera.main.backgroundColor = new Color(
-			Mathf.Sin(frequency*Time.timeSinceLevelLoad) * 0.5f + 0.5f,
-			Mathf.Sin(frequency*Time.timeSinceLevelLoad + 2) * 0.5f + 0.5f,
-			Mathf.Sin(frequency*Time.timeSinceLevelLoad + 4) * 0.5f + 0.5f
+			Mathf.Min(redMax / 255f, Mathf.Sin(frequency*Time.timeSinceLevelLoad) * 0.5f + 0.5f),
+			Mathf.Min(greenMax / 255f, Mathf.Sin(frequency*Time.timeSinceLevelLoad + 2) * 0.5f + 0.5f),
+			Mathf.Min(blueMax / 255f, Mathf.Sin(frequency*Time.timeSinceLevelLoad + 4) * 0.5f + 0.5f)
 		);
 	}
 }
