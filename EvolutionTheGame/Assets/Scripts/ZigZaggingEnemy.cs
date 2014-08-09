2cebaf85acd36ca4dbf58b28ecaa227a0a03597b
Daphne <daphneipp@gmail.com>
2014-08-08 23:30:30 -0400

    more enemy stuff. still don't look very good. spawn positions are shit.

--- EvolutionTheGame/Assets/Scripts/ZigZaggingEnemy.cs
+++ EvolutionTheGame/Assets/Scripts/ZigZaggingEnemy.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ZigZaggingEnemy : MonoBehaviour {
-	float fMagnitude = 0.75f;
-	Vector3 v3Axis = new Vector3(0f, 1f, 0);
+public class ZigZaggingEnemy : Enemy {
+	public float minSineMagnitude = 1;
+	public float maxSineMagnitude = 1.5f;
+
+	public float minSineFrequency = 1;
+	public float maxSineFrequency = 2;
 	
-	void Start() {
-		v3Axis.Normalize();
+	private float sineMagnitude;
+	private float sineFrequency;
+
+	/**
+	 * A bigger value means it will take longer to get to the player.
+	 */
+	private float timeToGetToPlayer = 100f;
+
+	private Vector3 startingPos;
+
+	public override void DoStart() {
+		base.DoStart();
+
+		startingPos = transform.position;
+
+		this.sineFrequency = Random.Range(minSineFrequency, maxSineFrequency);
+		this.sineMagnitude = Random.Range (minSineMagnitude, maxSineMagnitude);
 	}
 	
-	void Update () {
-		transform.localPosition = v3Axis * Mathf.Sin (Time.time) * fMagnitude;
+	override public void DoUpdate()
+	{
+		base.DoUpdate();
+
+		Vector3 positionOfPlayer = World.GetInstance().player.transform.position;
+
+		//Need to move toward the player.
+		Vector3 directionToPlayer = -(this.transform.position - positionOfPlayer);
+
+		//Sine movement should be perpendicular to the movement toward player.
+		Vector3 perpendicularToPlayer = new Vector3(-directionToPlayer.y, directionToPlayer.x, 0);
+		perpendicularToPlayer.Normalize();
+
+		//Move along the sine
+		transform.position = startingPos + (perpendicularToPlayer * Mathf.Sin (Time.time * sineFrequency) * sineMagnitude);
+
+		//Also move toward the player.
+		directionToPlayer.Normalize();
+		this.transform.position += (directionToPlayer / timeToGetToPlayer);
+		startingPos += (directionToPlayer / timeToGetToPlayer);
 	}
 }
