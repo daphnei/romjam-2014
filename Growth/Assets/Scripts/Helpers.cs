19cbc14f5d519fff1902089aea4a14c7aa8fbb33
Adam <adamgryu@gmail.com>
2014-08-09 12:21:31 -0400

    Added bullets sort of.

--- Growth/Assets/Scripts/Helpers.cs
+++ Growth/Assets/Scripts/Helpers.cs
@@ -13,6 +13,10 @@ public static class Helpers {
 		return new Vector2(vector.x, vector.y);
 	}
 
+	public static Vector3 ToVector3(this Vector2 vector) {
+		return new Vector3(vector.x, vector.y, 0);
+	}
+
 	public static Vector2 Rotate90DegreesCounterClockwise(this Vector2 vector) {
 		return new Vector2(-vector.y, vector.x);
 	}
