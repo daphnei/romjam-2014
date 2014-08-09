6bc7f0c39e1332792048e6f098e4fe7d201a7cda
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-08-08 21:32:54 -0400

    pulsing setup

---
+++ EvolutionTheGame/Assets/Scripts/PulseController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PulseController : MonoBehaviour {
+
+	public AudioSource song;
+
+	private Pulser[] pulsers;
+	private int samplesElapsed = 0;
+	private int lastSamples = 0;
+
+	// Use this for initialization
+	void Start () {
+		pulsers = this.GetComponentsInChildren<Pulser>();
+		song.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		samplesElapsed += song.timeSamples - lastSamples;
+		lastSamples = song.timeSamples;
+
+		if (samplesElapsed > 50000) {
+			samplesElapsed = 0;
+
+			foreach (Pulser pulser in this.pulsers) {
+				pulser.Pulse();
+			}
+		}
+	}
+}
