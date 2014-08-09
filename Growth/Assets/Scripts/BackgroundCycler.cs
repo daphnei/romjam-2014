10e700bb5d245da518cff32da118fec7857433f1
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-08-09 16:33:51 -0400

    Fix background cycling to be frame-dependent, not time-dependent.

--- Growth/Assets/Scripts/BackgroundCycler.cs
+++ Growth/Assets/Scripts/BackgroundCycler.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class BackgroundCycler : Pulser {
 
-	public float frequency = 0.1f;
-	public float cycleMovementSpeed = 2f;
+	public float frequency = 3f;
+	public float cycleDistance = 1f;
 
-	public int redMax = 255;
-	public int greenMax = 255;
-	public int blueMax = 255;
+	public int redMax = 200;
+	public int greenMax = 50;
+	public int blueMax = 200;
 
 	private float time;
 	private float nextTime;
@@ -16,20 +16,20 @@ public class BackgroundCycler : Pulser {
 	// Use this for initialization
 	void Start () {
 		PulseController.Instance.AddPulser(this);
-		time = Time.timeSinceLevelLoad;
-		nextTime = Time.timeSinceLevelLoad;
+		time = 0f;
+		nextTime = 0f;
 	}
 
-	public override void Pulse ()
+	public override void Pulse()
 	{
 		base.Pulse();
 
-		nextTime = Time.timeSinceLevelLoad;
+		nextTime += 0.25f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		time = Mathf.MoveTowards(time, nextTime, cycleMovementSpeed);
+		time = Mathf.MoveTowards(time, nextTime, 0.01f);
 
 		Camera.main.backgroundColor = new Color(
 			Mathf.Min(redMax / 255f, Mathf.Sin(frequency*time) * 0.5f + 0.5f),
