ed0253650707ef4ce6f2b42a3879f6564dad059b
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-08-09 15:57:44 -0400

    Adjust song pitch to movements.

--- Growth/Assets/Scripts/Pulse/PulseController.cs
+++ Growth/Assets/Scripts/Pulse/PulseController.cs
@@ -12,12 +12,16 @@ public class PulseController : SceneSingleton<PulseController> {
 	private int samplesElapsed = 0;
 	private int lastSamples = 0;
 
+	public float slowestTime = 1f;
+	public float fastestTime = 1.2f;
+	public float songLerpSpeed = 0.012f;
+
 	// Use this for initialization
 	void Start () {
 		pulsers = this.GetComponentsInChildren<Pulser>().ToList();
 
 		// nobody likes Newgrounds music
-		song.volume = 0;
+//		song.volume = 0;
 		song.Play();
 	}
 	
@@ -27,6 +31,8 @@ public class PulseController : SceneSingleton<PulseController> {
 		if (song.timeSamples < lastSamples) {
 			lastSamples = 0;
 		}
+
+		song.pitch = Mathf.MoveTowards(song.pitch, Mathf.Clamp(Time.timeScale, slowestTime, fastestTime), songLerpSpeed);
 
 		samplesElapsed += song.timeSamples - lastSamples;
 		lastSamples = song.timeSamples;
