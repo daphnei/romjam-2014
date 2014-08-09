05965c8c30b47ac185bc9a073edb22a72416a408
Daphne <daphneipp@gmail.com>
2014-08-09 12:07:22 -0400

    shitty code fix.

--- Growth/Assets/Scripts/Critter/Nutrient.cs
+++ Growth/Assets/Scripts/Critter/Nutrient.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Nutrient : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+public class Nutrient : Critter {
 	
 	// Update is called once per frame
-	void Update () {
-	
+	override public void DoUpdate () {
+		base.DoUpdate();
+
+		Vector3 positionOfPlayer = World.Instance.player.transform.position;
+		
+		//Move the enemy toward the center. This should maybe go faster as the enemy gets closer?
+		Vector3 directionToPlayer = -(this.transform.position - positionOfPlayer);
+		directionToPlayer.Normalize();
+				this.transform.position += Time.deltaTime * (directionToPlayer * 1.5f);
 	}
 }
