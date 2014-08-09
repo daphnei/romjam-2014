8b50690c03e09a52e45b8b5c804d474fa70dfcd4
Daphne <daphneipp@gmail.com>
2014-08-08 21:55:37 -0400

    fixed issue for Adam

--- EvolutionTheGame/Assets/Scripts/ZigZaggingEnemy.cs
+++ EvolutionTheGame/Assets/Scripts/ZigZaggingEnemy.cs
@@ -5,11 +5,12 @@ public class ZigZaggingEnemy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		this.rigidbody2D.velocity = new Vector2(1, 0);
+		this.transform.rotation = Quaternion.AngleAxis(20, Vector2.one);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
