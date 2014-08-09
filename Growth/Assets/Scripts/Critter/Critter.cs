a54f7ddf6f88167893defa2e8e3a52712d0b40b0
Max <mhuan13@gmail.com>
2014-08-09 12:18:03 -0400

    Merge branch 'master' of https://github.com/daphnei/romjam-2014
    Conflicts:
    	Growth/Assets/Prefabs/Player.prefab
    	Growth/Assets/Scenes/SampleLevel.unity

---
+++ Growth/Assets/Scripts/Critter/Critter.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * A critter is anything that spawns outside of the scene
+ * and will eventually come toward the player.
+ * 
+ * IE: Enemies, Nutrients
+ **/
+public class Critter : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		DoStart();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DoUpdate();
+	}
+
+	virtual public void DoStart()
+	{
+		ChooseSpawnPoint();
+	}
+
+	virtual public void DoUpdate() {
+		Vector3 positionOfPlayer = World.Instance.player.transform.position;
+
+		//At some point this can be replaced with a check for collision?
+		if (Mathf.Abs((this.transform.position - positionOfPlayer).magnitude) < 1.5)
+		{
+			HitThePlayer();
+		}
+	}
+
+	virtual protected void HitThePlayer()
+	{
+		Destroy(this.gameObject);
+	}                            
+
+	//Spawn on any of the edges of the screen.
+	virtual protected void ChooseSpawnPoint()
+	{
+		//How far off screen to do initial position;
+		int d = 3;
+
+		//Choose a spawn point on one of the edges of the viewport.
+		Vector2 spawnPoint;
+		if (Random.value >= 0.5)
+		{ 
+			//spawn with a random x
+			spawnPoint = new Vector2(
+				Random.Range(0, Camera.main.pixelWidth),
+				Random.value >= 0.5 ? -d : Camera.main.pixelHeight + d);
+			spawnPoint = Camera.main.ScreenToWorldPoint(spawnPoint);
+		}
+		else
+		{
+			//spawn with a random y
+			spawnPoint = new Vector2(
+				Random.value >= 0.5 ? -d : Camera.main.pixelWidth,
+				Random.Range(0, Camera.main.pixelHeight + d));
+			spawnPoint = Camera.main.ScreenToWorldPoint(spawnPoint);
+		}
+
+		this.transform.position = spawnPoint;
+	}
+}
