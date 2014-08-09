540b5438d321e6bfc8934b36b80891137152d88c
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-08-08 22:01:57 -0400

    Background cycler

---
+++ EvolutionTheGame/Assets/Scripts/BackgroundCycler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundCycler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera.main.backgroundColor = new Color(
+			Mathf.Sin(0.1f*Time.timeSinceLevelLoad) * 0.5f + 0.5f,
+			Mathf.Sin(0.1f*Time.timeSinceLevelLoad + 2) * 0.5f + 0.5f,
+			Mathf.Sin(0.1f*Time.timeSinceLevelLoad + 4) * 0.5f + 0.5f
+		);
+	}
+}
