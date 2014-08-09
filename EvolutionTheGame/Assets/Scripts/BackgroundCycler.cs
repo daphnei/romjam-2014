c0145bcb5e35aa7a76959cffe9873a11ce2896ad
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-08-08 22:03:27 -0400

    expose frequency of background color changes

--- EvolutionTheGame/Assets/Scripts/BackgroundCycler.cs
+++ EvolutionTheGame/Assets/Scripts/BackgroundCycler.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class BackgroundCycler : MonoBehaviour {
 
+	public float frequency = 0.1f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,9 +13,9 @@ public class BackgroundCycler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Camera.main.backgroundColor = new Color(
-			Mathf.Sin(0.1f*Time.timeSinceLevelLoad) * 0.5f + 0.5f,
-			Mathf.Sin(0.1f*Time.timeSinceLevelLoad + 2) * 0.5f + 0.5f,
-			Mathf.Sin(0.1f*Time.timeSinceLevelLoad + 4) * 0.5f + 0.5f
+			Mathf.Sin(frequency*Time.timeSinceLevelLoad) * 0.5f + 0.5f,
+			Mathf.Sin(frequency*Time.timeSinceLevelLoad + 2) * 0.5f + 0.5f,
+			Mathf.Sin(frequency*Time.timeSinceLevelLoad + 4) * 0.5f + 0.5f
 		);
 	}
 }
