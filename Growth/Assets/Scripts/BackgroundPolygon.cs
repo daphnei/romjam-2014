4049befee27ec6ce8e8ee7a1fa297e4a44bede17
Max <mhuan13@gmail.com>
2014-08-09 23:32:46 -0400

    Optimized some shizzle my nizzle
    that's racist

--- Growth/Assets/Scripts/BackgroundPolygon.cs
+++ Growth/Assets/Scripts/BackgroundPolygon.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class BackgroundPolygon : MonoBehaviour {
 
+	MeshFilter playerPolygon;
+	MeshFilter thisMesh;
+
 	// Use this for initialization
 	void Start () {
-	
+		playerPolygon = GameObject.Find("Player/Polygon").GetComponent<MeshFilter>();
+		thisMesh = this.GetComponent<MeshFilter>();
+		thisMesh.mesh = playerPolygon.mesh;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if ( Object.ReferenceEquals(thisMesh.mesh, playerPolygon.mesh) ) {
+			thisMesh.mesh = playerPolygon.mesh;
+		}
 	}
 }
