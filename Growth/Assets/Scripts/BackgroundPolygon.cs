4a58ec3d705eb8360211482f53171324dc6d2d02
Daphne <daphneipp@gmail.com>
2014-08-10 14:00:46 -0400

    fixed menus

--- Growth/Assets/Scripts/BackgroundPolygon.cs
+++ Growth/Assets/Scripts/BackgroundPolygon.cs
@@ -3,19 +3,32 @@ using System.Collections;
 
 public class BackgroundPolygon : MonoBehaviour {
 
-	MeshFilter playerPolygon;
-	MeshFilter thisMesh;
+	private MeshFilter playerPolygon = null;
+	protected MeshFilter thisMesh = null;
+
+	public int defaultNumVertices = 7;
 
 	// Use this for initialization
 	void Start () {
-		playerPolygon = GameObject.Find("Player/Polygon").GetComponent<MeshFilter>();
-		thisMesh = this.GetComponent<MeshFilter>();
-		thisMesh.mesh = playerPolygon.mesh;
+		Player player = World.Instance.player;
+		if (player != null)
+		{
+			this.playerPolygon = player.polygon.GetComponent<MeshFilter>();
+			this.thisMesh = this.GetComponent<MeshFilter>();
+			this.thisMesh.mesh = playerPolygon.mesh;
+		}
+		else
+		{
+			//No player to get a meshfilter from.
+			this.thisMesh = this.GetComponent<MeshFilter>();
+			this.thisMesh.mesh = PolygonMaker.makeMesh(defaultNumVertices);
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if ( !Object.ReferenceEquals(thisMesh.mesh, playerPolygon.mesh) ) {
+		if ( playerPolygon != null && Object.ReferenceEquals(thisMesh.mesh, playerPolygon.mesh) ) {
 			thisMesh.mesh = playerPolygon.mesh;
 		}
 	}
